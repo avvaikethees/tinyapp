78459a80622534302588f94dc8c506ae6a12015e
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-29 06:03:17 +0000

    Create a form and a new route

--- express_server.js
+++ express_server.js
@@ -1,13 +1,21 @@
 const express = require ('express');
 const app = express();
+
 const PORT = 8080; // defualt port 8080
 
 app.set("view engine", "ejs");
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
 
 app.get('/', (req, res) => {
   res.send("Hello")
@@ -24,6 +32,15 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+})
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 })
 
 app.get('/urls/:shortURL', (req, res)=> {
