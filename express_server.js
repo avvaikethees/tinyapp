c36ca72dc08568f6ae0a279195cc42d93b6d28a2
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-01 00:48:29 +0000

    created a register page and a cookie to store user ID info

--- express_server.js
+++ express_server.js
@@ -1,25 +1,53 @@
 const express = require ('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+// Server Setup --------------------------------------
 const PORT = 8080; // defualt port 8080
-
+const app = express();
 app.set("view engine", "ejs");
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}))
 
-const cookieParser = require('cookie-parser');
+//Middleware --------------------------------------
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
 
+//Data --------------------------------------
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {
+const usersDatabase = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
+//function to generate a random alphanumeric 6 character string 
+function generateRandomString () {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
 }
-// all my browse routes
+
+// function to check if email already exists
+const emailAlreadyExists = (usersDatabase, newEmail) => {
+  for (key in usersDatabase) {
+    if (usersDatabase[key]["email"] === newEmail) {
+      return true
+    }
+  }
+  return false; 
+};
+
+// Routes --------------------------------------
+//View -------------------------------------
 app.get('/', (req, res) => {
   res.send("Hello")
 });
@@ -33,17 +61,24 @@ app.get('/hello', (req, res) => {
 })
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const userID = req.cookies["user_id"]
+  const templateVars = { urls: urlDatabase, userID: req.cookies["user_id"], user:usersDatabase[userID]};
   res.render('urls_index', templateVars);
 })
 
 app.get('/urls/new', (req, res) => {
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"]}
+  const userID = req.cookies["user_id"]
+  const templateVars = {urls: urlDatabase, userID: req.cookies["user_id"], user:usersDatabase[userID]}
   res.render('urls_new', templateVars);
 })
 
+app.get('/urls/register', (req, res) => {
+  res.render('urls_register')
+})
+
 app.get('/urls/:shortURL', (req, res)=> {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+  const userID = req.cookies["user_id"]
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], userID: req.cookies["user_id"], user: usersDatabase[userID]};
   console.log(templateVars)
   res.render("urls_show", templateVars);
 })
@@ -54,7 +89,7 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL)
 })
 
-//all my updates 
+// Action ----------------------------------------------------
 app.post("/urls", (req, res) => {
   const longBodyURL = req.body.longURL;
   const shortURL = generateRandomString();
@@ -71,7 +106,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls")
 })
 
-//what happens when you click on the edit button 
+// ** what happens when you click on the edit button  **
 app.post("/urls/:shortURL", (req, res) => {
   console.log("Post request fired")
   const longURL = req.body.newURL
@@ -84,20 +119,54 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls")
 })
 
-// What happens when you click on the login button
+// ** What happens when you click on the login button **
 app.post('/login', (req, res) => {
-  res.cookie("username", req.body["username"])
+  //res.cookie("username", req.body["username"])
   //console.log(req.body)
   res.redirect("/urls")
 })
 
-// What happens when you click on the logout button 
+// ** What happens when you click on the logout button **
 app.post('/logout', (req, res) => {
-  res.clearCookie("username")
+  //res.clearCookie("username")
   res.redirect("/urls")
 })
 
-//setting up the listener 
+// ** What happens after the registration form **
+app.post('/register', (req,res)=>{
+
+  let email = req.body.email;
+  let password = req.body.password;
+  // if email or password is missing
+  if (!email || !password) {
+    res.status(400).json({message: 'Please enter an email/password'})
+  
+  //email already exists in database
+  } else if (emailAlreadyExists(usersDatabase, email)) {
+    res.status(400).json({message: 'Email is already in the system'})
+  } else {
+
+    const userID = generateRandomString();
+    const storeuser = {
+      id: userID, 
+      email: email,
+      password: password,
+    }
+  
+
+  //this adds the new user to the global users object 
+  usersDatabase[userID] = storeuser 
+  //console.log(storeuser)
+  console.log(usersDatabase)
+  
+  // setting a user_id cookie containing the user's newly generated ID 
+  //res.cookie(name, value)
+  res.cookie("user_id", storeuser.id)
+  res.redirect("/urls")
+  }
+})
+
+//setting up the listener -------------------------------------------------------
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 }); 
